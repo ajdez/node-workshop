e67bef5e5746e38f989a3e0b672bc7274cd02088
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-16 20:11:51 +0000

    A wild interval has appeared!

--- hello-world.js
+++ hello-world.js
@@ -1,8 +1,21 @@
 // First Program
 
-console.log("Hello World!");
 
+console.log("Hello World!");
 setTimeout(x => console.log("Hello World Again!!"), 10000);
 
 
 //A wild interval has appeared!
+
+
+function printIt (){
+    console.log("hi");
+    setTimeout(printIt, 2000);
+}
+
+printIt();
+
+
+
+
+
