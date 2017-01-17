35a45f21c64d354f0226d03c68d7fdda550f85e3
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-17 15:04:10 +0000

    complete

--- guessing-game.js
+++ guessing-game.js
@@ -2,22 +2,35 @@ var prompt = require('prompt');
 
 prompt.start();
 
+var randomNumber = Math.floor(Math.random()*101);
+var count = 0;
 function guessingGame(){
-    var count = 0;
-    while(count < 4)
-    prompt.get("number", function(err, result){
-        var randomNumber = Math.floor(Math.random()*101);
-        if(result.number === randomNumber){
-            console.log("Wow you got it!");
-            return;
-        }
-        else if (result.number < randomNumber){
-            console.log("Try guessing a higher number");
-            count++;
-        }
-        else if (result.number > randomNumber){
-            console.log("Try guessing a lower number.");
-            count++;
-        }
-    })
+    if(count < 4){
+        prompt.get("number", function(err, result){
+            if(err){
+                console.log("Something went wrong buddy");
+            }
+            else{
+                if(result.number === randomNumber){
+                    console.log("Wow you got it!");
+                    return;
+                }
+                else if (result.number < randomNumber){
+                    console.log("Try guessing a higher number");
+                    count++;
+                    guessingGame();
+                }
+                else if (result.number > randomNumber){
+                    console.log("Try guessing a lower number.");
+                    count++;
+                    guessingGame();
+                }
+            } 
+        }) 
+    }
+    else{
+        console.log("Out of try's");
+    }
 }
+
+guessingGame();
