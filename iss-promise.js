8fce89c87352e668bcb5813c36a07f562ab56e9c
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-18 23:29:15 +0000

    Completed iss-promise using promises

--- iss-promise.js
+++ iss-promise.js
@@ -3,7 +3,10 @@ var request = require('request-promise');
 var urlPromise = request("http://api.open-notify.org/iss-now.json");
 
 urlPromise.then(function(result){
-    var issData = JSON.parse(result);
-    var issLat = issData.iss_position.latitude;
-    console.log(issLat);
+    var issData = JSON.parse(result);                      //issData is holding the JSON object
+    var issLat = issData.iss_position.latitude;         
+    var issLng = issData.iss_position.longitude;
+    
+    console.log("ISS latitude is : " + issLat + " and longitue : " + issLng);
+    
 })
