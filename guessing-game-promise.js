ccfaf0ac87641ff79613dc67696925a5efdb2885
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-19 01:48:46 +0000

    have the same issue with command line with guessing game

--- guessing-game-promise.js
+++ guessing-game-promise.js
@@ -1,27 +1,34 @@
 var prompt = require('prompt-promise');
-var count = 5;
+var count = 10;
 
 
 var randomNumber = Math.floor(Math.random()*101);
 
 function numberGuessingGame(){
-    var numberGuess = prompt("Guess a number");
+    var numGuess = prompt("Guess a number : ");
     if( count > 0){
-        return numberGuess.then(function(){
+        return numGuess.then(function(guess){
+            var numberGuess = +guess;
             if(numberGuess === randomNumber){
                 console.log("you won!")
                 return;
             }
             else if(numberGuess < randomNumber){
                 console.log("Try guessing a higher number");
+                count--;
                 numberGuessingGame();
             }
             else if (numberGuess > randomNumber){
                 console.log("Try guessing a lower number")
+                count--;
                 numberGuessingGame();
             }
-        })
+        })// Then
     }
-    
-    
+    else{
+        console.log("You lose");
+        return;
+    }
 }
+
+numberGuessingGame();
