c3f38dcbc2301ba649cbf3ae33b4dd97be233b3c
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-19 01:25:10 +0000

    reworked on iss-augmented-promise

--- iss-augmented-promise.js
+++ iss-augmented-promise.js
@@ -1,41 +1,9 @@
+// Radian info ////////////////////////////////////////////////////////////
+
 Number.prototype.toRadians = function() {
     return this * Math.PI / 180;
   }
-
-
-var request = require('request-promise');
-var prompt = require('prompt-promise');
-
-var urlISS = request("http://api.open-notify.org/iss-now.json");
-
-
-var issLat , issLng;
-
-urlISS.then(function(result){
-    var issData = JSON.parse(result);
-    issLat = +issData.iss_position.latitude;
-    issLng = +issData.iss_position.longitude;
-    
-    var answerCity = prompt("What city are you in?  :  ");
-    
-    answerCity.then(function(cityResult){
-        var userCity = cityResult;
-        var urlCityString = "https://maps.googleapis.com/maps/api/geocode/json?address=" + cityResult;
-        var urlCityPromise = request(urlCityString);
-        
-        urlCityPromise.then(function(cityResult){
-            var cityData = JSON.parse(cityResult);
-            var cityLat = +cityData.results[0].geometry.location.lat;
-            var cityLng = +cityData.results[0].geometry.location.lng;
-            
-            var distanceToCity = calculateDistance(issLat, issLng, cityLat, cityLng).toFixed(2);
-            console.log("Distance from the 'Internation Space Station' to " + userCity + " is : " +distanceToCity);
-        })
-    })
-})
-
-
-
+  
 function calculateDistance (lat1, lon1, lat2, lon2){
     var R = 6371e3; // metres
     var φ1 = lat1.toRadians();
@@ -52,4 +20,49 @@ function calculateDistance (lat1, lon1, lat2, lon2){
     
     return d;
 }
+//////////////////////////////// radian info on top ////////////////////////////////
 
+var request = require('request-promise');
+var prompt = require('prompt-promise');
+
+var urlISS = request("http://api.open-notify.org/iss-now.json");
+
+function computeDistancePromise(){
+    var issLat, issLng;
+    
+    return urlISS.then(function(result){
+        var issData = JSON.parse(result);
+        issLat = +(issData.iss_position.latitude);
+        issLng = +(issData.iss_position.longitude);
+        
+        return prompt("what city are you in? : ")
+    })
+    .then(function(cityResult){
+        var userCity = cityResult;
+        var urlCityString = "https://maps.googleapis.com/maps/api/geocode/json?address=" + cityResult;
+        var urlCityPromise = request(urlCityString);
+        return urlCityPromise;
+    })
+    .then(function(cityURL){
+        var cityData = JSON.parse(cityURL);
+        var cityLat = +(cityData.results[0].geometry.location.lat);
+        var cityLng = +(cityData.results[0].geometry.location.lng);
+        
+        var distanceToCity = calculateDistance(issLat, issLng, cityLat, cityLng).toFixed(2);
+        return distanceToCity;
+    })
+    .catch(function(err){
+        console.log("something went wrong", err);
+    })
+}
+
+
+
+
+
+computeDistancePromise().then(function(result){
+    console.log(result)
+})
+.catch(function(err){
+    console.log(err);
+})
